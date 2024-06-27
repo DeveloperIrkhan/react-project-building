5faf34ef7f22b4cf0e7d14c748bfed7fe0f5faa4
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2024-06-27 15:23:53 +0400

    Creating new project for generating new password

---
+++ React 1/React-app=2/MyApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-red=200 bg-red-300 text-3xl font-bold underline">234</div>
+    </>
+  )
+}
+
+export default App
