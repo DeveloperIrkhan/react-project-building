a6f288ddc4f64cd76703204ae4009dfd86d54b2a
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2024-07-04 12:43:02 +0400

    Currency Converter using custom Hook

--- React 1/React-shuffle-search/react-search-shuffle/src/App.jsx
+++ React 1/React-shuffle-search/react-search-shuffle/src/App.jsx
@@ -2,41 +2,14 @@ import { useCallback, useState } from 'react'
 import './App.css'
 import ListValues from './assets/ListValues'
 import Application from './Application'
+import CallbackHook from './CallbackHook'
 
 function App() {
-  const [dark, setDark] = useState(false)
-  const [number, setNumber] = useState(0)
-  const theam = {
-    backgroundColor: dark ? "yellow" : "blue",
-    color: dark ? "blue" : "yellow",
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-  const getList = useCallback(() => {
-    return [number, number + 1, number + 2, number + 3, number + 4]
-  },[number])
+  
   return (
-    <div style={theam}>
-      <div className="flex flex-col">
-        <div className="">
-          <input
-          type='number'
-            value={number}
-            onChange={e => setNumber(parseInt(e.target.value))}
-          />
-        </div>
-        <div className="">
-          <button style={{ padding: "13px", borderRadius: '12px', backgroundColor: "red", color: "white" }}
-            onClick={() => setDark(prevDark => !prevDark)}>Change Theam
-          </button>
-        </div>
-        <div className="p-4">
-          <ListValues getElements={getList} />
-        </div>
-      </div>
+    <div>
+      {/* <CallbackHook/> */}
+      {/* <Application/> */}
     </div>
   )
 }
